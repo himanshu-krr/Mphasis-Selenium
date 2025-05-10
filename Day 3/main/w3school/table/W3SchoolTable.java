a8c876ec4b4bd477fc413e6d0880ad4a9f1077f9
himanshu-krr <83543102+himanshu-krr@users.noreply.github.com>
2025-05-10 12:27:25 +0530

    Update W3SchoolTable.java

--- Day 3/main/w3school/table/W3SchoolTable.java
+++ Day 3/main/w3school/table/W3SchoolTable.java
@@ -29,6 +29,39 @@ public class W3SchoolTable {
 //		WebElement ele = wd.findElement(By.xpath("//*[@id=\"customers\"]/tbody/tr[2]/td[2]"));
 //		return ele;
 //	}
+
+	public void practicing()
+	{
+		List <WebElement> rl = wd.findElements(By.xpath("(//table[@class ='ws-table-all'])/tbody/tr"));
+		System.out.println(rl.size());
+		
+		List <WebElement> rc = wd.findElements(By.xpath("(//table[@class ='ws-table-all'])/tbody/tr[1]/th"));
+		System.out.println(rc.size());
+		
+//		for(int r=2;r<=rl.size();r++)
+//		{
+//			for(int c=1;c<=rc.size();c++)
+//			{
+//				String d = wd.findElement(By.xpath("//table/tbody/tr[" + r + "]/td[" + c + "]")).getText();
+//				System.out.print(d);
+//			}
+//		}
+		// Loop through rows including the header row
+		for (int r = 1; r <= rl.size(); r++) { // Start from the first row (header row)
+			for (int c = 1; c <= rc.size(); c++) {
+				// For the header row (tr[1]), use 'th', otherwise use 'td'
+				String cellData;
+				if (r == 1) { // Header row
+					cellData = wd.findElement(By.xpath("(//table[@class ='ws-table-all'])/tbody/tr[" + r + "]/th[" + c + "]")).getText();
+				} else { // Data rows
+					cellData = wd.findElement(By.xpath("(//table[@class ='ws-table-all'])/tbody/tr[" + r + "]/td[" + c + "]")).getText();
+				}
+				System.out.print(cellData + "\t"); // Add a tab for formatting between columns
+			}
+			System.out.println();
+		}// Move to the next line after printing all columns in a row
+	}
+	
 	
 	public void getTable()
 	{
